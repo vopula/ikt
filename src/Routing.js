8217008665a14f8b85469b846ac0cd551eada541
vopula <alkasidikprawira12@gmail.com>
2025-02-26 08:50:28 +0700

    new status checking

---
+++ src/Routing.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.js";
+import Test from "./pages/testing.js";
+import DashboardUser from "./pages/DashboardUser.js";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Route untuk halaman Login */}
+        <Route path="/" element={<Test />} />
+        {/* Route untuk halaman Dashboard */}
+        <Route path="/dashboardUser" element={<DashboardUser />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './Routing';
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
